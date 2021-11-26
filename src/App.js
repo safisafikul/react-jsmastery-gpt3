85a84f882eca8d41274f9e28a684bafe60779ced
unknown <safisafikuls@gmail.com>
2021-11-27 00:11:55 +0600

    features done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
-import { CTA, Brand, Navbar } from './components'
-import "./App.css"
-
+import {
+	Footer,
+	Blog,
+	Possibility,
+	Features,
+	WhatGPT3,
+	Header,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
 	return (
